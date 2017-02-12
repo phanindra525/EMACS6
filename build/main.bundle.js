ff15b0d6dc3cca79e5a6b1a2be3902ba78544f15
Phani Madala <phanindra525@gmail.com>
2017-02-12 12:39:37 -0600

    es6 features

--- build/main.bundle.js
+++ build/main.bundle.js
@@ -1,55 +1,87 @@
 'use strict';
 
-var calculateMonthlyPayment = function calculateMonthlyPayment(principal, years, rate) {
-    if (rate) {
-        var monthlyRate = rate / 100 / 12;
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Mortgage = function () {
+    function Mortgage(principal, years, rate) {
+        _classCallCheck(this, Mortgage);
+
+        this.principal = principal;
+        this.years = years;
+        this.rate = rate;
     }
 
-    // var to let 
-    var monthlyPayment = principal * monthlyRate / (1 - Math.pow(1 / (1 + monthlyRate), years * 12));
-    return { monthlyRate: monthlyRate, monthlyPayment: monthlyPayment };
-};
+    _createClass(Mortgage, [{
+        key: 'calculateMonthlyPayment',
+        value: function calculateMonthlyPayment() {
+            var monthlyRate = this.rate / 100 / 12;
 
-var calculateTotalPayment = function calculateTotalPayment(monthlyPayment, years) {
-    return years * 12 * monthlyPayment;
-};
-
-var calculateAmortization = function calculateAmortization(principal, years, rate) {
-    var _calculateMonthlyPaym = calculateMonthlyPayment(principal, years, rate),
-        monthlyRate = _calculateMonthlyPaym.monthlyRate,
-        monthlyPayment = _calculateMonthlyPaym.monthlyPayment;
-
-    var balance = principal;
-    var amortization = [];
-    for (var y = 0; y < years; y++) {
-        var interestY = 0; //Interest payment for year y
-        var principalY = 0; //Principal payment for year y
-        for (var m = 0; m < 12; m++) {
-            var interestM = balance * monthlyRate; //Interest payment for month m
-            var principalM = monthlyPayment - interestM; //Principal payment for month m
-            interestY = interestY + interestM;
-            principalY = principalY + principalM;
-            balance = balance - principalM;
+            // var to let 
+            var monthlyPayment = this.principal * monthlyRate / (1 - Math.pow(1 / (1 + monthlyRate), this.years * 12));
+            return { monthlyRate: monthlyRate, monthlyPayment: monthlyPayment };
         }
-        amortization.push({ principalY: principalY, interestY: interestY, balance: balance });
-    }
-    return { monthlyPayment: monthlyPayment, monthlyRate: monthlyRate, amortization: amortization };
-};
+    }, {
+        key: 'calculateTotalPayment',
+        value: function calculateTotalPayment(monthlyPayment) {
+            return this.years * 12 * monthlyPayment;
+        }
+
+        // calculateAmortization () {
+        //     let {monthlyRate, monthlyPayment} = this.calculateMonthlyPayment();
+        //     let balance = principal;
+        //     let amortization = [];
+        //     for (let y=0; y<years; y++) {
+        //         let interestY = 0;  //Interest payment for year y
+        //         let principalY = 0; //Principal payment for year y
+        //         for (let m=0; m<12; m++) {
+        //             let interestM = balance * monthlyRate;       //Interest payment for month m
+        //             let principalM = monthlyPayment - interestM; //Principal payment for month m
+        //             interestY = interestY + interestM;
+        //             principalY = principalY + principalM;
+        //             balance = balance - principalM;
+        //         }
+        //         amortization.push({principalY, interestY, balance});
+        //     }
+        //     return {monthlyPayment, monthlyRate, amortization};
+        // }
+
+    }]);
+
+    return Mortgage;
+}();
 
 document.getElementById('calcBtn').addEventListener('click', function () {
     var principal = document.getElementById("principal").value;
     var years = document.getElementById("years").value;
     var rate = document.getElementById("rate").value;
 
-    var _calculateAmortizatio = calculateAmortization(principal, years, rate),
-        monthlyPayment = _calculateAmortizatio.monthlyPayment,
-        monthlyRate = _calculateAmortizatio.monthlyRate,
-        amortization = _calculateAmortizatio.amortization;
+    var mortgage = new Mortgage(principal, years, rate);
+
+    var _mortgage$calculateMo = mortgage.calculateMonthlyPayment(),
+        monthlyRate = _mortgage$calculateMo.monthlyRate,
+        monthlyPayment = _mortgage$calculateMo.monthlyPayment;
 
     document.getElementById("monthlyPayment").innerHTML = monthlyPayment.toFixed(2);
-    amortization.forEach(function (month) {
-        return console.log(month);
+    //amortization.forEach(month => console.log(month));
+    var totalPayment = mortgage.calculateTotalPayment(monthlyPayment);
+    document.getElementById("totalPayment").innerHTML = 'this is my total payment :(\n        cx dsf\n         ' + totalPayment + ' ';
+
+    generateRandomNumber().then(function (result) {
+        console.log("success : " + result);
+    }).catch(function (error) {
+        console.log("error: " + error);
     });
-    var totalPayment = calculateTotalPayment(monthlyPayment, years);
-    document.getElementById("totalPayment").innerHTML = totalPayment;
 });
+
+function generateRandomNumber() {
+    return new Promise(function (resolve, reject) {
+        var randomNumber = Math.floor(Math.random() * 10 + 1);
+        if (randomNumber > 5) {
+            resolve(randomNumber);
+        } else {
+            reject(randomNumber);
+        }
+    });
+}
--- js/main.js
+++ js/main.js
@@ -1,45 +1,85 @@
-let calculateMonthlyPayment = (principal, years, rate)=>{
-    if (rate) {
-        var monthlyRate = rate / 100 / 12;
+class Mortgage {
+    
+
+    constructor(principal, years, rate) {
+        this.principal = principal;
+        this.years = years;
+        this.rate = rate;
+        
     }
 
-    // var to let 
-    let monthlyPayment = principal * monthlyRate / (1 - (Math.pow(1 / (1 + monthlyRate), years * 12)));
-    return {monthlyRate, monthlyPayment};
-};
 
-let calculateTotalPayment= (monthlyPayment, years) =>  {
-    return years *12 * monthlyPayment;
+    calculateMonthlyPayment(){
+            var monthlyRate = this.rate / 100 / 12;
+
+        // var to let 
+        let monthlyPayment = this.principal *monthlyRate / (1 - (Math.pow(1 / (1 +monthlyRate),  this.years * 12)));
+        return {monthlyRate, monthlyPayment};
+    };
+
+    calculateTotalPayment(monthlyPayment)  {
+        return this.years *12 * monthlyPayment;
+    }
+
+    // calculateAmortization () {
+    //     let {monthlyRate, monthlyPayment} = this.calculateMonthlyPayment();
+    //     let balance = principal;
+    //     let amortization = [];
+    //     for (let y=0; y<years; y++) {
+    //         let interestY = 0;  //Interest payment for year y
+    //         let principalY = 0; //Principal payment for year y
+    //         for (let m=0; m<12; m++) {
+    //             let interestM = balance * monthlyRate;       //Interest payment for month m
+    //             let principalM = monthlyPayment - interestM; //Principal payment for month m
+    //             interestY = interestY + interestM;
+    //             principalY = principalY + principalM;
+    //             balance = balance - principalM;
+    //         }
+    //         amortization.push({principalY, interestY, balance});
+    //     }
+    //     return {monthlyPayment, monthlyRate, amortization};
+    // }
+
 }
 
-let calculateAmortization = (principal, years, rate) => {
-    let {monthlyRate, monthlyPayment} = calculateMonthlyPayment(principal, years, rate);
-    let balance = principal;
-    let amortization = [];
-    for (let y=0; y<years; y++) {
-        let interestY = 0;  //Interest payment for year y
-        let principalY = 0; //Principal payment for year y
-        for (let m=0; m<12; m++) {
-            let interestM = balance * monthlyRate;       //Interest payment for month m
-            let principalM = monthlyPayment - interestM; //Principal payment for month m
-            interestY = interestY + interestM;
-            principalY = principalY + principalM;
-            balance = balance - principalM;
-        }
-        amortization.push({principalY, interestY, balance});
-    }
-    return {monthlyPayment, monthlyRate, amortization};
-};
+
+
 
 
 document.getElementById('calcBtn').addEventListener('click', () => {
     let principal = document.getElementById("principal").value;
     let years = document.getElementById("years").value;
     let rate = document.getElementById("rate").value;
-    let {monthlyPayment, monthlyRate, amortization} = calculateAmortization(principal, years, rate);
+
+    let mortgage = new Mortgage(principal, years, rate);
+    let {monthlyRate, monthlyPayment} = mortgage.calculateMonthlyPayment();
     document.getElementById("monthlyPayment").innerHTML = monthlyPayment.toFixed(2);
-    amortization.forEach(month => console.log(month));
-    let totalPayment = calculateTotalPayment(monthlyPayment, years);
-    document.getElementById("totalPayment").innerHTML = totalPayment;
+    //amortization.forEach(month => console.log(month));
+    let totalPayment = mortgage.calculateTotalPayment(monthlyPayment);
+    document.getElementById("totalPayment").innerHTML = `this is my total payment :(
+        cx dsf
+         ${totalPayment} `;
+
+    generateRandomNumber().then((result) => {
+        console.log("success : "+result);
+    }).catch ((error)=> {
+        console.log("error: "+ error);
+    })
 
 });
+
+
+function generateRandomNumber() {
+    return new Promise((resolve, reject) => {
+        var randomNumber = Math.floor((Math.random() * 10) + 1)
+        if(randomNumber > 5) {
+            resolve(randomNumber);
+
+        } else {
+            reject (randomNumber);
+        }
+    })
+}
+
+
+
